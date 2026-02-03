16b16fe708e52a2b108530ca90bc8fe26867935d
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2026-02-03 11:20:58 +0700

    Add files via upload

---
+++ lib_booking/src/main/java/doan/j2ee/lib_booking/LibBookingApplication.java
@@ -0,0 +1,13 @@
+package doan.j2ee.lib_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibBookingApplication.class, args);
+	}
+
+}
---
+++ lib_booking/src/test/java/doan/j2ee/lib_booking/LibBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package doan.j2ee.lib_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
